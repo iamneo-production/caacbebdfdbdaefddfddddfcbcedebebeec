7638e7f78e4f18ce622d0a2b33e057e1eedfa357
Iamneo <techadmin@iamneo.ai>
2023-08-14 11:33:34 +0000

    Backup Commit

--- Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
+++ Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.AfterTest;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.*;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -28,43 +28,33 @@ public class AppTest {
 
     @Test
     public void testSnapdealLogin() {
-        
+      
         driver.get(url);
 
-	}
-	@Test
-    public void tc2() {
         
         WebElement signInButton = driver.findElement(By.xpath("//div[@class='accountInner']/span[contains(text(),'Sign In')]"));
         Actions actions = new Actions(driver);
         actions.moveToElement(signInButton).click().perform();
 
-	}
-	@Test
-    public void tc3() {
-        
         WebElement emailInput = driver.findElement(By.id("userName"));
         emailInput.sendKeys("your_valid_email@example.com");
         WebElement continueButton = driver.findElement(By.id("checkUser"));
         continueButton.click();
-	}
-	@Test
-    public void tc4() {
-        
-        
+
+      
         WebElement passwordInput = driver.findElement(By.id("j_password_login_uc"));
         passwordInput.sendKeys("your_valid_password");
         WebElement loginButton = driver.findElement(By.id("submitLoginUC"));
         loginButton.click();
 
-      
+        
         try {
             WebElement successMessage = driver.findElement(By.xpath("//div[@class='accountInner']/span[contains(text(),'My Account')]"));
             System.out.println("User is logged in successfully!");
         } catch (Exception e) {
             System.out.println("Login failed!");
         }
-	}
+    }
 
     @AfterTest
     public void afterTest() {
